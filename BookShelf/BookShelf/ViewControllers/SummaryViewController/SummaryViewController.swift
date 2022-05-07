d71175604bba89589877194aeb2dfed33a81bf2f
Aditya Krishna <adityakrishna@Adityas-MacBook-Pro.local>
2022-05-06 20:58:41 -0400

    Minor UI change in cell

--- BookShelf/BookShelf/ViewControllers/SummaryViewController/SummaryViewController.swift
+++ BookShelf/BookShelf/ViewControllers/SummaryViewController/SummaryViewController.swift
@@ -29,8 +29,15 @@ class SummaryViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
+        setupCollectionView()
         setupCallbacks()
         setupDataFields()
+        setCollectionViewHeight()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        setCollectionViewHeight()
     }
     
     private func setupCallbacks() {
@@ -52,12 +59,77 @@ class SummaryViewController: UIViewController {
         shippingLabel.text = String(viewModel.cartDetails.shippingPrice ?? 9)
         totalAmountLabel.text = String(viewModel.cartDetails.totalPrice ?? 0)
     }
+    
+    private func setupCollectionView() {
+        booksCollectionView.register(UINib(nibName: "CartBooksCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "CartBooksCollectionViewCell")
+        booksCollectionView.delegate = self
+        booksCollectionView.dataSource = self
+    }
+
+    private func setCollectionViewHeight() {
+        booksCollectionView.reloadData()
+        booksCollectionView.layoutIfNeeded()
+        booksCollectionViewHeightConstraint.constant = booksCollectionView.contentSize.height
+    }
 }
 
 // MARK: - @IBAction methods
 extension SummaryViewController {
     @IBAction func didTapConfirmOrder(_ sender: Any) {
         viewModel.confirmOrder()
+    }
+}
+
+// MARK: - UICollectionViewDelegate and UICollectionViewDataSource methods
+extension SummaryViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return viewModel.cartDetails.orderItems?.count ?? 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CartBooksCollectionViewCell", for: indexPath) as? CartBooksCollectionViewCell else {
+            fatalError("Could not dequeue cell")
+        }
+        
+        let product = viewModel.cartDetails.orderItems?[indexPath.item]
+        cell.bookImageView.kf.setImage(with: URL(string: product?.image ?? ""))
+        cell.titleLabel.text = product?.name
+        cell.titleLabel.sizeToFit()
+        cell.quantityTextField.text = String(product?.qty ?? 0)
+        cell.quantityTextField.isEnabled = false
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+        addCellShadow(cell: cell)
+    }
+    
+    private func addCellShadow(cell: UICollectionViewCell) {
+        cell.contentView.layer.cornerRadius = 6.0
+        cell.contentView.layer.borderWidth = 1.0
+        cell.contentView.layer.borderColor = UIColor.clear.cgColor
+        cell.contentView.layer.masksToBounds = true
+        cell.layer.shadowColor = UIColor.darkGray.cgColor
+        cell.layer.shadowOffset = CGSize(width: 0, height: 2.0)
+        cell.layer.shadowRadius = 6
+        cell.layer.shadowOpacity = 0.5
+        cell.layer.masksToBounds = false
+        cell.layer.shadowPath = UIBezierPath(roundedRect: cell.bounds, cornerRadius: cell.contentView.layer.cornerRadius).cgPath
+    }
+}
+
+extension SummaryViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        CGSize(width: 300, height: CartBooksCollectionViewCell.CELL_HEIGHT*1.5)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        20
     }
 }
 
